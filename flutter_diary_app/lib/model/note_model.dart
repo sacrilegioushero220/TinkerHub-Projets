48986576182085791e113a24acc474379a8bb894
Ash-394 <aswini.at100@gmail.com>
2022-02-27 17:43:46 +0530

    resolved errors

--- flutter_diary_app/lib/model/note_model.dart
+++ flutter_diary_app/lib/model/note_model.dart
@@ -2,12 +2,13 @@ class NoteModel {
   int id;
   String title;
   String body;
+  // ignore: non_constant_identifier_names
   DateTime creationDate;
+  // ignore: non_constant_identifier_names
   NoteModel(
-      {
-        required this.id ,
-      required this.title ,
-      required this.body ,
+      {required this.id,
+      required this.title,
+      required this.body,
       required this.creationDate});
 
   //function to convert our item into a map
