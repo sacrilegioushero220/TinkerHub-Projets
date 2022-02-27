06f6ff298c79580a63e26656a384ac27d6498b07
Ash-394 <aswini.at100@gmail.com>
2022-02-27 15:27:23 +0530

    updated the errors

---
+++ flutter_diary_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_diary_app/db/database_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //We will use routes to navigate between screens
+      initialRoute: "/",
+      routes: {"/": (context) => HomeScreen()},
+      title: 'A Diary Entry App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //getting all entries
+  getNotes() async {
+    final notes = await DatabaseProvider.db.getNotes();
+    return notes;
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Your Diary Entries"),
+    ),
+    body: FutureBuilder(
+      future: getNotes(),
+      builder: (context, noteData) {
+        switch (noteData.connectionState) {
+          case ConnectionState.waiting:
+            {
+              return Center(child: CircularProgressIndicator());
+            }
+          case ConnectionState.done:
+            {
+              //check value we got is not null
+              if (noteData.data == Null) {
+                return Center(
+                  child: Text("You don't have any entries yet, create one"),
+                );
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    itemCount: noteData.data.length,
+                    itemBuilder: (context, index) {
+                      //setting the items
+                      String title = noteData.data[index]['title'];
+                      String body = noteData.data[index]['body'];
+                      String creation_date =
+                          noteData.data[index]['creation_date'];
+                      int id = noteData.data[index]['id'];
+                      return Card(
+                        child: ListTile(
+                          title: Text(title),
+                          subtitle: Text(body),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+            }
+        }
+      },
+    ),
+  );
+}
--- flutter_diary_app/test/widget_test.dart
+++ flutter_diary_app/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_diary_app/screens/main.dart';
+import 'package:flutter_diary_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
