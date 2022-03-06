373005daa378b82dc151c88a5b7f582763477c77
sacrilegioushero220 <sandweepsabu@gmail.com>
2022-03-06 23:32:07 +0530

    added title and note adding features

--- flutter_diary_app/lib/screens/add_note.dart
+++ flutter_diary_app/lib/screens/add_note.dart
@@ -12,7 +12,31 @@ class _AddNoteState extends State<AddNote> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add new Note"),
+        title: Text("Add new entry"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Title",
+              ),
+              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Note",
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
