64db117de8e9a9f3e1773b5bf377161cc3210e36
Ash-394 <aswini.at100@gmail.com>
2022-03-10 11:45:43 +0530

    updated display

--- flutter_diary_app/lib/screens/display_note.dart
+++ flutter_diary_app/lib/screens/display_note.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_diary_app/db/database_provider.dart';
 import 'package:flutter_diary_app/model/note_model.dart';
+import 'package:intl/intl.dart';
 
 class DisplayNote extends StatelessWidget {
   const DisplayNote({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int i;
     final NoteModel note =
         ModalRoute.of(context).settings.arguments as NoteModel;
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('Your note!'),
+          title: Text(DateFormat('EEE, dd-MM-yyyy').format(note.creationDate)),
           actions: [
             IconButton(
               icon: Icon(Icons.delete),
               onPressed: () {
-               DatabaseProvider.db.deleteNote(note.id);
+                DatabaseProvider.db.deleteNote(note.id);
                 Navigator.pushNamedAndRemoveUntil(
                     context, '/', (route) => false);
               },
